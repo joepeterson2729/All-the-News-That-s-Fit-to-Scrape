0e919be18e84d18701e226b4ddc962ff271ec046
Joe P <joe.peterson2729@gmail.com>
2019-08-25 18:56:38 -0500

    set up handlebars files

--- server.js
+++ server.js
@@ -4,11 +4,11 @@
 // dependencies
 // =============================================================
 const express = require('express'),
-      exphbs = require('express-handlebars'),
-      bodyParser = require('body-parser'),
-      logger = require('morgan'),
-      mongoose = require('mongoose'),
-      methodOverride = require('method-override');
+    exphbs = require('express-handlebars'),
+    bodyParser = require('body-parser'),
+    logger = require('morgan'),
+    mongoose = require('mongoose'),
+    methodOverride = require('method-override');
 
 // set up express app
 // =============================================================
@@ -17,7 +17,7 @@ let app = express();
 
 app
     .use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended:true }))
+    .use(bodyParser.urlencoded({ extended: true }))
     .use(bodyParser.text())
     .use(bodyParser.json({ type: 'application/vnd.api+json' }))
     .use(methodOverride('_method'))
@@ -41,15 +41,15 @@ mongoose.connect(dbURI, { useNewUrlParser: true });
 const db = mongoose.connection;
 
 // Show any mongoose errors
-db.on("error", function(error) {
+db.on("error", function (error) {
     console.log("Mongoose Error: ", error);
 });
 
 // Once logged in to the db through mongoose, log a success message
-db.once("open", function() {
+db.once("open", function () {
     console.log("Mongoose connection successful.");
     // start the server, listen on port 3000
-    app.listen(PORT, function() {
+    app.listen(PORT, function () {
         console.log("App running on port " + PORT);
     });
 });
